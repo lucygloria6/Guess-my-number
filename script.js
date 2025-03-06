68ed75cd16046ccefe3e36af25ae8981b8d1560d
Anogwi Oluchi Gloria <lucygloria6@gmail.com>
2025-03-06 21:52:58 +0100

    guess my number
    guess my number game

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// document.querySelector('.message').textContent = '✅Correct Number!';
+
+// document.querySelector('.number') .textContent= 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+}
+
+const hiddenNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // when there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔No Number!';
+    displayMessage('⛔No Number!');
+   
+    //   when player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '✅Correct Number!';
+    displayMessage('✅Correct Number!');
+    
+    // document.querySelector('.number').textContent = secretNumber;
+    hiddenNumber(secretNumber);
+   
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highScore').textContent = highScore; 
+    }
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent = guess > secretNumber ? '↗ Too high!': '↘Too low!' ;
+      displayMessage(guess > secretNumber ? '↗ Too high!': '↘Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '😰You lost the game!';
+      displayMessage('😰You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // when guess is too high
+//   }else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '↗ Too high!' ;
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '😰You lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   // when guess is too low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '↘Too low!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '😰You lost the game!';
+//     }
+    
+//   }
+   
+ });
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  // document.querySelector('.number').textContent = '?';
+  hiddenNumber('?');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  
+})
